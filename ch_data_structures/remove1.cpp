231072724be14f740a177a5afed02d9b10812d46
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2023-12-12 23:38:52 +0100

    Update remove1.cpp

--- ch_data_structures/remove1.cpp
+++ ch_data_structures/remove1.cpp
@@ -13,7 +13,31 @@ int main()
 {
     std::vector v1 = { 6, 4, 7, 3, 9, 0, 1, 5 };
 
-    std::remove()
+    auto removed_item = std::remove(v1.begin(), v1.end(), 9);
+
+    if (removed_item!=v1.end())
+        v1.erase(removed_item);
 
     print_vector(v1);
+
+    removed_item = std::remove_if(v1.begin(), v1.end(), [](int v){return v % 2 == 0;});
+
+    if (removed_item!=v1.end())
+        v1.erase(removed_item);
+
+    print_vector(v1);
+
+    std::vector v2 = { 4, 5, 7, 4, 3, 3, 7, 7, 4, 5};
+    //std::vector v2 = { 1, 2, 3, 4, 5 };
+
+    auto last = std::unique(v2.begin(), v2.end());
+    v2.erase(last, v2.end());
+
+    std::sort(v2.begin(), v2.end());
+
+    last = std::unique(v2.begin(), v2.end());
+    v2.erase(last, v2.end());
+
+    print_vector(v2);
+
 }
