6e8c350cc46906ffd5735a59f2a216ea964a046e
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2023-12-19 00:04:29 +0100

    Update vtk3.cpp

--- ch_vtk/vtk3.cpp
+++ ch_vtk/vtk3.cpp
@@ -1,63 +1,118 @@
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+namespace fs = std::filesystem;
+
 #include <vtkActor.h>
+#include <vtkAssembly.h>
 #include <vtkAxesActor.h>
 #include <vtkCamera.h>
+#include <vtkClipDataSet.h>
+#include <vtkClipPolyData.h>
+#include <vtkCubeAxesActor.h>
 #include <vtkCylinderSource.h>
 #include <vtkDataSetMapper.h>
 #include <vtkGenericDataObjectReader.h>
+#include <vtkLegendScaleActor.h>
+#include <vtkLookupTable.h>
 #include <vtkNamedColors.h>
 #include <vtkNew.h>
+#include <vtkPlane.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkProperty.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
+#include <vtkScalarBarActor.h>
+#include <vtkShaderProperty.h>
 #include <vtkStructuredGrid.h>
+#include <vtkTextProperty.h>
 #include <vtkTransform.h>
 #include <vtkUnstructuredGrid.h>
-#include <vtkLookupTable.h>
-#include <vtkScalarBarActor.h>
-#include <vtkLegendScaleActor.h>
-#include <vtkCubeAxesActor.h>
-#include <vtkTextProperty.h>
-#include <vtkPlane.h>
-#include <vtkClipPolyData.h>
-#include <vtkClipDataSet.h>
-#include <vtkShaderProperty.h>
 
 #include <array>
 
-vtkSmartPointer<vtkActor> loadModel(const std::string filename, vtkLookupTable* lut)
+vtkSmartPointer<vtkUnstructuredGrid> loadModel(const std::string filename)
 {
     vtkNew<vtkGenericDataObjectReader> reader;
     reader->SetFileName(filename.c_str());
     reader->Update();
-
-    auto dataActor = vtkSmartPointer<vtkActor>::New();
-    auto dataSetMapper = vtkSmartPointer<vtkDataSetMapper>::New();
 
     if (reader->IsFileUnstructuredGrid())
     {
         std::cout << "output is unstructured grid," << std::endl;
         auto model = reader->GetUnstructuredGridOutput();
         std::cout << "   output has " << model->GetNumberOfPoints() << " points."
-            << std::endl;
+                  << std::endl;
 
-        dataSetMapper->SetInputData(model);
-        dataSetMapper->SelectColorArray("Resistivity(log10)");
-        dataSetMapper->ScalarVisibilityOn();
-        dataSetMapper->SetLookupTable(lut);
-        dataSetMapper->SetScalarRange(model->GetScalarRange());
-
-        dataActor->SetMapper(dataSetMapper);
-
-        return dataActor;
+        return model;
     }
     return nullptr;
+}
+
+vtkSmartPointer<vtkActor> createFullModelActor(vtkSmartPointer<vtkUnstructuredGrid> model, vtkLookupTable* lut)
+{
+    auto dataActor = vtkSmartPointer<vtkActor>::New();
+    auto dataSetMapper = vtkSmartPointer<vtkDataSetMapper>::New();
+
+    dataSetMapper->SetInputData(model);
+    dataSetMapper->SelectColorArray("Resistivity(log10)");
+    dataSetMapper->ScalarVisibilityOn();
+    dataSetMapper->SetLookupTable(lut);
+    // dataSetMapper->SetScalarRange(model->GetScalarRange());
+    dataSetMapper->SetScalarRange(2.0, 3.0);
+
+    dataActor->SetMapper(dataSetMapper);
+    dataActor->GetProperty()->SetOpacity(0.3);
+    dataActor->SetOrientation(-90.0, 0.0, 0.0);
+
+    return dataActor;
+}
+
+vtkSmartPointer<vtkActor> createClippedModelActor(vtkSmartPointer<vtkUnstructuredGrid> model, vtkLookupTable* lut)
+{
+    auto plane = vtkSmartPointer<vtkPlane>::New();
+    plane->SetOrigin(model->GetCenter());
+    plane->SetNormal(-1.0, 0.0, 0.0);
+
+    auto clipper = vtkSmartPointer<vtkClipDataSet>::New();
+    clipper->SetClipFunction(plane);
+    clipper->SetInputData(model);
+    clipper->SetValue(0.0);
+    clipper->GenerateClippedOutputOn();
+    clipper->Update();
+
+    auto clipMapper = vtkSmartPointer<vtkDataSetMapper>::New();
+    clipMapper->SelectColorArray("Resistivity(log10)");
+    clipMapper->ScalarVisibilityOn();
+    clipMapper->SetLookupTable(lut);
+    // clipMapper->SetScalarRange(model->GetScalarRange());
+    clipMapper->SetScalarRange(2.0, 3.0);
+    clipMapper->SetInputData(clipper->GetOutput());
+
+    auto clipActor = vtkSmartPointer<vtkActor>::New();
+    clipActor->SetMapper(clipMapper);
+    clipActor->SetOrientation(-90.0, 0.0, 0.0);
+
+    return clipActor;
+}
+
+std::vector<std::string> findVTKFiles()
+{
+    std::vector<std::string> file_list;
+
+    for (const auto& entry : fs::directory_iterator(".\\data"))
+        file_list.push_back(entry.path().string());
+
+    return file_list;
 }
 
 int main(int, char*[])
 {
     vtkNew<vtkNamedColors> colors;
+
+    // Create a lookup table
 
     vtkNew<vtkLookupTable> lut;
     lut->SetNumberOfColors(256);
@@ -65,59 +120,38 @@ int main(int, char*[])
     lut->Build();
 
     // Set the background color.
+
     std::array<unsigned char, 4> bkg { { 26, 51, 102, 255 } };
     colors->SetColor("BkgColor", bkg.data());
 
-    vtkNew<vtkActor> dataActor;
-    vtkNew<vtkDataSetMapper> dataSetMapper;
+    // Load models and create actors.
 
-    vtkNew<vtkGenericDataObjectReader> reader;
-    reader->SetFileName("dcinv.result_0.vtk");
-    reader->Update();
+    vtkNew<vtkAssembly> assembly;
 
-    vtkUnstructuredGrid* model = nullptr;
+    auto file_list = findVTKFiles();
 
-    if (reader->IsFileUnstructuredGrid())
+    auto count = 0;
+
+    for (auto& filename : file_list)
     {
-        std::cout << "output is unstructured grid," << std::endl;
-        model = reader->GetUnstructuredGridOutput();
-        std::cout << "   output has " << model->GetNumberOfPoints() << " points."
-                  << std::endl;
+        std::cout << "Loading: " << filename << std::endl;
+        auto model = loadModel(filename);
+        auto modelActor = createFullModelActor(model, lut);
+        auto clippedActor = createClippedModelActor(model, lut);
 
-        dataSetMapper->SetInputData(model);
-        dataSetMapper->SelectColorArray("Resistivity(log10)");
-        dataSetMapper->ScalarVisibilityOn();
-        dataSetMapper->SetLookupTable(lut);
-        dataSetMapper->SetScalarRange(model->GetScalarRange());
+        assembly->AddPart(modelActor);
+        assembly->AddPart(clippedActor);
 
-        dataActor->SetMapper(dataSetMapper);
-        dataActor->GetProperty()->SetOpacity(0.2);
+        if (count == 1)
+            break;
+        count++;
     }
 
-    if (model == nullptr)
-        return -1;
-
-    vtkNew<vtkPlane> plane;
-    plane->SetOrigin(dataActor->GetMapper()->GetInput()->GetCenter());
-    //plane->SetOrigin(model->GetCenter());
-    plane->SetNormal(0.0, -1.0, 0.0);
-
-    vtkNew<vtkClipDataSet> clipper;
-    clipper->SetClipFunction(plane);
-    clipper->SetInputData(model);
-    clipper->SetValue(0.0);
-    clipper->GenerateClippedOutputOn();
-    clipper->Update();
-
-    vtkNew<vtkDataSetMapper> clipMapper;
-    clipMapper->SelectColorArray("Resistivity(log10)");
-    clipMapper->ScalarVisibilityOn();
-    clipMapper->SetLookupTable(lut);
-    clipMapper->SetScalarRange(model->GetScalarRange());
-    clipMapper->SetInputData(clipper->GetOutput());
-
-    vtkNew<vtkActor> clipActor;
-    clipActor->SetMapper(clipMapper);
+    /*
+    auto model = loadModel("dcinv.result_0.vtk");
+    auto modelActor = createFullModelActor(model, lut);
+    auto clippedActor = createClippedModelActor(model, lut);
+    */
 
     vtkNew<vtkScalarBarActor> scalarBar;
     scalarBar->SetLookupTable(lut);
@@ -133,71 +167,36 @@ int main(int, char*[])
     scalarBar->UnconstrainedFontSizeOff();
     scalarBar->SetOrientationToVertical();
 
-
-
-
-    if (reader->IsFileStructuredGrid())
-    {
-        std::cout << "output is structured grid," << std::endl;
-        auto output = reader->GetStructuredGridOutput();
-        std::cout << "   output has " << output->GetNumberOfPoints() << " points."
-                  << std::endl;
-    }
-
-    // The renderer generates the image
-    // which is then displayed on the render window.
-    // It can be thought of as a scene to which the actor is added
-    vtkNew<vtkRenderer> renderer;
-    // renderer->AddActor(cylinderActor);
-    // renderer->AddActor(axes);
-
     vtkNew<vtkCubeAxesActor> cubeAxesActor;
     cubeAxesActor->SetUseTextActor3D(1);
-    cubeAxesActor->SetBounds(dataSetMapper->GetBounds());
-    cubeAxesActor->SetCamera(renderer->GetActiveCamera());
-    //cubeAxesActor->GetTitleTextProperty(0)->SetColor(axis1Color.GetData());
-    //cubeAxesActor->GetTitleTextProperty(0)->SetFontSize(48);
-    //cubeAxesActor->GetLabelTextProperty(0)->SetColor(axis1Color.GetData());
-
-    //cubeAxesActor->GetTitleTextProperty(1)->SetColor(axis2Color.GetData());
-    //cubeAxesActor->GetLabelTextProperty(1)->SetColor(axis2Color.GetData());
-
-    //cubeAxesActor->GetTitleTextProperty(2)->SetColor(axis3Color.GetData());
-    //cubeAxesActor->GetLabelTextProperty(2)->SetColor(axis3Color.GetData());
+    cubeAxesActor->SetBounds(assembly->GetBounds());
+    // cubeAxesActor->SetOrientation(90.0, 0.0, 0.0);
 
     cubeAxesActor->DrawXGridlinesOn();
     cubeAxesActor->DrawYGridlinesOn();
     cubeAxesActor->DrawZGridlinesOn();
     cubeAxesActor->SetGridLineLocation(cubeAxesActor->VTK_GRID_LINES_FURTHEST);
 
+    vtkNew<vtkRenderer> renderer;
+
+    cubeAxesActor->SetCamera(renderer->GetActiveCamera());
+
+    renderer->AddActor(assembly);
     renderer->AddActor(cubeAxesActor);
-
-
-    renderer->AddActor(clipActor);
-    renderer->AddActor(dataActor);
     renderer->AddActor2D(scalarBar);
 
-
     renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
-    // Zoom in a little by accessing the camera and invoking its "Zoom" method.
     renderer->ResetCamera();
-    // renderer->GetActiveCamera()->Zoom(1.5);
     renderer->GetActiveCamera()->SetParallelProjection(false);
 
-    // The render window is the actual GUI window
-    // that appears on the computer screen
     vtkNew<vtkRenderWindow> renderWindow;
     renderWindow->SetSize(800, 800);
     renderWindow->AddRenderer(renderer);
-    renderWindow->SetWindowName("Cylinder");
+    renderWindow->SetWindowName("VTK Example");
 
-    // The render window interactor captures mouse events
-    // and will perform appropriate camera or actor manipulation
-    // depending on the nature of the events.
     vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
     renderWindowInteractor->SetRenderWindow(renderWindow);
 
-    // This starts the event loop and as a side effect causes an initial render.
     renderWindow->Render();
     renderWindowInteractor->Start();
 
