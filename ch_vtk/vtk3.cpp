d12037ab36d67b03bce20ad2c288c7b4a3166443
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2023-12-19 16:36:46 +0100

    Work on vtk3 example.

--- ch_vtk/vtk3.cpp
+++ ch_vtk/vtk3.cpp
@@ -1,6 +1,9 @@
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <array>
 
 namespace fs = std::filesystem;
 
@@ -30,10 +33,87 @@ namespace fs = std::filesystem;
 #include <vtkTextProperty.h>
 #include <vtkTransform.h>
 #include <vtkUnstructuredGrid.h>
+#include <vtkCallbackCommand.h>
+#include <vtkProp3DCollection.h>
+#include <vtkColorSeries.h>
 
-#include <array>
 
-vtkSmartPointer<vtkUnstructuredGrid> loadModel(const std::string filename)
+class TimerCallback : public vtkCallbackCommand
+{
+public:
+    TimerCallback() = default;
+
+    static TimerCallback *New()
+    {
+        TimerCallback *cb = new TimerCallback;
+        cb->m_timerCount = 0;
+        return cb;
+    }
+
+    virtual void Execute(vtkObject *caller, unsigned long eventId, void *vtkNotUsed(callData))
+    {
+        if (vtkCommand::TimerEvent == eventId)
+        {
+            ++this->m_timerCount;
+        }
+
+        auto models = modelAssembly->GetParts();
+        auto clippedModels = clippedAssembly->GetParts();
+
+        models->InitTraversal();
+        clippedModels->InitTraversal();
+        for (auto i=0; i<models->GetNumberOfItems(); i++)
+        {
+            auto currentModel = models->GetNextProp();
+            auto currentClippedModel = clippedModels->GetNextProp();
+
+            if (i == m_currentStep)
+            {
+                currentModel->VisibilityOn();
+                currentClippedModel->VisibilityOn();
+            }
+            else
+            {
+                currentModel->VisibilityOff();
+                currentClippedModel->VisibilityOff();
+            }
+        }
+        m_currentStep++;
+        
+        if (m_currentStep == models->GetNumberOfItems())
+            m_currentStep = 0;
+
+        //actor->SetPosition(this->m_timerCount, this->m_timerCount, 0);        
+        
+        if (this->m_timerCount < this->maxCount)
+        {
+            vtkRenderWindowInteractor *iren =
+                dynamic_cast<vtkRenderWindowInteractor *>(caller);
+            iren->GetRenderWindow()->Render();
+        }
+        else
+        {
+            vtkRenderWindowInteractor *iren =
+                dynamic_cast<vtkRenderWindowInteractor *>(caller);
+            if (this->timerId > -1)
+            {
+                iren->DestroyTimer(this->timerId);
+            }
+        }
+    }
+
+private:
+    int m_timerCount = 0;
+    int m_currentStep = 0;
+public:
+    vtkAssembly* modelAssembly = nullptr;
+    vtkAssembly* clippedAssembly = nullptr;
+    int timerId = 0;
+    int maxCount = -1;
+};
+
+vtkSmartPointer<vtkUnstructuredGrid>
+loadModel(const std::string filename)
 {
     vtkNew<vtkGenericDataObjectReader> reader;
     reader->SetFileName(filename.c_str());
@@ -51,7 +131,7 @@ vtkSmartPointer<vtkUnstructuredGrid> loadModel(const std::string filename)
     return nullptr;
 }
 
-vtkSmartPointer<vtkActor> createFullModelActor(vtkSmartPointer<vtkUnstructuredGrid> model, vtkLookupTable* lut)
+vtkSmartPointer<vtkActor> createFullModelActor(vtkSmartPointer<vtkUnstructuredGrid> model, vtkLookupTable *lut)
 {
     auto dataActor = vtkSmartPointer<vtkActor>::New();
     auto dataSetMapper = vtkSmartPointer<vtkDataSetMapper>::New();
@@ -70,11 +150,11 @@ vtkSmartPointer<vtkActor> createFullModelActor(vtkSmartPointer<vtkUnstructuredGr
     return dataActor;
 }
 
-vtkSmartPointer<vtkActor> createClippedModelActor(vtkSmartPointer<vtkUnstructuredGrid> model, vtkLookupTable* lut)
+vtkSmartPointer<vtkActor> createClippedModelActor(vtkSmartPointer<vtkUnstructuredGrid> model, vtkLookupTable *lut)
 {
     auto plane = vtkSmartPointer<vtkPlane>::New();
     plane->SetOrigin(model->GetCenter());
-    plane->SetNormal(-1.0, 0.0, 0.0);
+    plane->SetNormal(0.0, -1.0, 0.0);
 
     auto clipper = vtkSmartPointer<vtkClipDataSet>::New();
     clipper->SetClipFunction(plane);
@@ -102,56 +182,73 @@ std::vector<std::string> findVTKFiles()
 {
     std::vector<std::string> file_list;
 
-    for (const auto& entry : fs::directory_iterator(".\\data"))
+    for (const auto &entry : fs::directory_iterator("./data"))
         file_list.push_back(entry.path().string());
+
+    std::ranges::sort(file_list, [](const std::string& a, const std::string& b) 
+    {
+        size_t pos1 = a.find_last_of('_');
+        int num1 = std::stoi(a.substr(pos1 + 1));
+        size_t pos2 = b.find_last_of('_');
+        int num2 = std::stoi(b.substr(pos2 + 1));
+        
+        return num1 < num2;
+    });
 
     return file_list;
 }
 
-int main(int, char*[])
+int main(int, char *[])
 {
     vtkNew<vtkNamedColors> colors;
 
     // Create a lookup table
 
+    vtkNew<vtkColorSeries> colorSeries;
+    //colorSeries->SetColorScheme(vtkColorSeries::BREWER_DIVERGING_SPECTRAL_11);
+    colorSeries->SetColorScheme(vtkColorSeries::BREWER_DIVERGING_PURPLE_ORANGE_11);
+    
+
+    vtkNew<vtkLookupTable> lut;
+    lut->SetNumberOfColors(256);
+    colorSeries->BuildLookupTable(lut, vtkColorSeries::ORDINAL);
+
+    /*
     vtkNew<vtkLookupTable> lut;
     lut->SetNumberOfColors(256);
     lut->SetHueRange(0.667, 0.0);
     lut->Build();
+    */
 
     // Set the background color.
 
-    std::array<unsigned char, 4> bkg { { 26, 51, 102, 255 } };
+    std::array<unsigned char, 4> bkg{{26, 51, 102, 255}};
     colors->SetColor("BkgColor", bkg.data());
 
     // Load models and create actors.
 
-    vtkNew<vtkAssembly> assembly;
+    vtkNew<vtkAssembly> fullModelAssembly;
+    vtkNew<vtkAssembly> clippedModelAssembly;
 
     auto file_list = findVTKFiles();
 
     auto count = 0;
 
-    for (auto& filename : file_list)
+    for (auto &filename : file_list)
     {
         std::cout << "Loading: " << filename << std::endl;
         auto model = loadModel(filename);
+
         auto modelActor = createFullModelActor(model, lut);
         auto clippedActor = createClippedModelActor(model, lut);
 
-        assembly->AddPart(modelActor);
-        assembly->AddPart(clippedActor);
+        fullModelAssembly->AddPart(modelActor);
+        clippedModelAssembly->AddPart(clippedActor);
 
-        if (count == 1)
+        if (count == 10)
             break;
         count++;
     }
-
-    /*
-    auto model = loadModel("dcinv.result_0.vtk");
-    auto modelActor = createFullModelActor(model, lut);
-    auto clippedActor = createClippedModelActor(model, lut);
-    */
 
     vtkNew<vtkScalarBarActor> scalarBar;
     scalarBar->SetLookupTable(lut);
@@ -169,7 +266,7 @@ int main(int, char*[])
 
     vtkNew<vtkCubeAxesActor> cubeAxesActor;
     cubeAxesActor->SetUseTextActor3D(1);
-    cubeAxesActor->SetBounds(assembly->GetBounds());
+    cubeAxesActor->SetBounds(fullModelAssembly->GetBounds());
     // cubeAxesActor->SetOrientation(90.0, 0.0, 0.0);
 
     cubeAxesActor->DrawXGridlinesOn();
@@ -181,7 +278,8 @@ int main(int, char*[])
 
     cubeAxesActor->SetCamera(renderer->GetActiveCamera());
 
-    renderer->AddActor(assembly);
+    renderer->AddActor(fullModelAssembly);
+    renderer->AddActor(clippedModelAssembly);
     renderer->AddActor(cubeAxesActor);
     renderer->AddActor2D(scalarBar);
 
@@ -196,6 +294,15 @@ int main(int, char*[])
 
     vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
     renderWindowInteractor->SetRenderWindow(renderWindow);
+    renderWindowInteractor->Initialize();
+
+    vtkNew<TimerCallback> cb;
+    cb->modelAssembly = fullModelAssembly;
+    cb->clippedAssembly = clippedModelAssembly;
+    renderWindowInteractor->AddObserver(vtkCommand::TimerEvent, cb);
+    int timerId = renderWindowInteractor->CreateRepeatingTimer(200);
+    cb->maxCount = 1000000;
+    cb->timerId = timerId;
 
     renderWindow->Render();
     renderWindowInteractor->Start();
---
+++ ln-modules.sh
@@ -0,0 +1,1 @@
+ml GCC/12.2.0 OpenMPI/4.1.4 VTK/9.2.6 Qt5/5.15.7 CMake
